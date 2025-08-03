9e8593cca0e2264f8c6b9638ea99507b8549c308
Bibek2307 <bibek.mukherjee2307@gmail.com>
2025-08-03 17:38:57 +0530

    commit

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import "../style.css"
+
 
 const Meals = () => {
+
+    const [item,setItem]=useState([])
+
+    useEffect(()=>{
+        axios
+        .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+        .then((res)=>{
+            setItem(res.data.meals)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })    
+    },[])
+
+const itemlist= item.map(({strMeal,strMealThumb,idMeal})=>{
+    return(
+        <section className='card'>
+            <img src={strMealThumb} alt={strMeal} />
+            <section className='content'>
+                <p>{strMeal}</p>
+                <p>#{idMeal}</p>
+            </section>
+        </section>
+    )
+})
+
   return (
-    <div>
-      Meals
+    <div className='container'>
+     {itemlist}
     </div>
   )
 }
 
-export default Meals
+export default Meals;
